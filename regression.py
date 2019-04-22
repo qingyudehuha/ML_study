0075b8268d1c976ab0f3ea115b909a06efbc7995
青鱼的呼哈 <33363193+qingyudehuha@users.noreply.github.com>
2019-04-22 20:18:31 +0800

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,4 +1,30 @@
-# 最小二乘法实现多元线性回归
+# 用不同方法实现多元线性回归
 
 import pandas as pd
 import numpy as np
+from sklearn import datasets
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# 用最小二乘法实现
+class LinearRegression1:
+
+    def __init__(self):
+        X = datasets.load_boston().data
+        y = datasets.load_boston().target
+        self.X = X
+        self.y = y
+
+
+    def fit(self):
+        self.X = np.insert(self.X,0,1,axis=1)
+        self.coef_ = np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(self.y)
+        # self.intercept_ = np.sum(self.y - self.X.dot(self.coef_))
+        return self.coef_
+            # , self.intercept_
+
+
+    def predict(self, x):
+        a = self.fit().dot(x)
+        return a.reshape(-1,1)
